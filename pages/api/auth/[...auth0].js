a7c5d64743f79dfc77f738e8fc9417da4aec1fec
Deryk DeGuzman <deryk.deguzman@gmail.com>
2022-08-20 11:48:36 -0700

    clean up auth

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,14 +1,3 @@
-// pages/api/auth/[...auth0].js
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
-
-/*export default handleAuth({
-  async login(req, res) {
-    try {
-      await handleLogin(req, res, { returnTo: '/me' });
-      logger('Redirecting to login');
-    } catch (error) {
-    }
-  }
-});*/
 
 export default handleAuth();
