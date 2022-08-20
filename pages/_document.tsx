7706e81388f7956066b1d5610f22ef3b4d0595f4
Deryk DeGuzman <deryk.deguzman@gmail.com>
2022-08-19 18:30:21 -0700

    start analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,19 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&display=swap" rel="stylesheet"></link>
         
         <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;400;600&display=swap" rel="stylesheet"></link>
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-W6S90RE5C4"></script>
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-W6S90RE5C4');
+          `,
+            }}
+          />
       </Head>
       <body>
         <Main />
